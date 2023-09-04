04302fc00c4a478a66bf4eb28d59beb5e95f9aa5
Dudu Popkhadze <dudupopkhadze@gmail.com>
2023-09-04 21:03:28 +0400

    Add cors

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,40 @@ mod  compiler;
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde;
 
-use rocket::http::Status;
+use rocket::http::{Status, Method, Header};
 use serde::Serialize;
 use rocket::serde::json::Json;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        if request.method() == Method::Options {
+            response.set_status(Status::NoContent);
+            response.set_header(Header::new(
+                "Access-Control-Allow-Methods",
+                "POST, PATCH, GET, DELETE",
+            ));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        }
+
+        response.set_header(Header::new(
+            "Access-Control-Allow-Origin",
+            "*",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 
 #[derive(Serialize, Deserialize)]
@@ -32,5 +63,5 @@ fn compile(code_payload: Json<CodePayload>) -> Result<Json<compiler::CompileResu
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, compile])
+    rocket::build().attach(CORS).mount("/", routes![index, compile])
 }
